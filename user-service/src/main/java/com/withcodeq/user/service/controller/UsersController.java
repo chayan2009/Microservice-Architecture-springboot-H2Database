3bc8d9751f73375e938663990617fa04a04c0aef
chayan2009 <chayanchowdhury@gmail.com>
2024-10-27 22:01:53 +0530

    Swagger Documentation Integrated (#5)

--- user-service/src/main/java/com/withcodeq/user/service/controller/UsersController.java
+++ user-service/src/main/java/com/withcodeq/user/service/controller/UsersController.java
@@ -1,12 +1,12 @@
 package com.withcodeq.user.service.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.withcodeq.user.service.entity.Users;
 import com.withcodeq.user.service.service.UsersService;
@@ -18,20 +18,39 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/users")
 @Slf4j
 public class UsersController {
-    
+
     @Autowired
     private UsersService usersService;
 
+    @Operation(summary = "Add a new user", description = "Saves a new user into the system")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "User saved successfully",
+                content = @Content(mediaType = "application/json",
+                schema = @Schema(implementation = Users.class))),
+        @ApiResponse(responseCode = "400", description = "Invalid input",
+                content = @Content),
+        @ApiResponse(responseCode = "500", description = "Internal server error",
+                content = @Content)
+    })
     @PostMapping("/add")
-    public Users saveUser(@RequestBody Users users){
-        log.info("inside saveinfo of UsersController");
+    public Users saveUser(@RequestBody Users users) {
+        log.info("inside saveUser method of UsersController");
         return usersService.saveUser(users);
     }
 
+    @Operation(summary = "Get user with department", description = "Fetches user details along with associated department information by user ID")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "User found",
+                content = @Content(mediaType = "application/json",
+                schema = @Schema(implementation = ResponseTemplateVo.class))),
+        @ApiResponse(responseCode = "404", description = "User not found",
+                content = @Content),
+        @ApiResponse(responseCode = "500", description = "Internal server error",
+                content = @Content)
+    })
     @GetMapping("/{id}")
-    public ResponseTemplateVo getUserWithDepartment(@PathVariable("id")long userId){
-        log.info("inside getUserWithDepartment of UsersController");
+    public ResponseTemplateVo getUserWithDepartment(@PathVariable("id") long userId) {
+        log.info("inside getUserWithDepartment method of UsersController");
         return usersService.getUserWithDepartment(userId);
     }
-
 }
